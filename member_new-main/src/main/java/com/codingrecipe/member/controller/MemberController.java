79a3ebeb1258c177b3c3b4db3c405ece68034dc1
Hyeseon kim <65377057+gptjs1124@users.noreply.github.com>
2024-05-28 10:08:08 +0900

    springBoot Member 연습

---
+++ member_new-main/src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.codingrecipe.member.controller;
+import com.codingrecipe.member.MemberApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class MemberController {
+    public static void main(String[] args){
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
+
+
+
+
+
+
+
+
+
